3fa6ac18bc44b134d85e96152d6f0480e67d82b6
Amran <amx109@gmail.com>
2014-01-08 00:04:12 +0100

    working with TLS and external sound file. added readme and LICENSE

---
+++ README.md
@@ -0,0 +1,9 @@
+a simple python script that:
+
+ gets list of emails from inbox IMAP folder (via TLS)
+ plays sound for each email greater than last mail UID
+ stores highest email UID
+
+so note: on first run it will play a sound for every mail in the inbox since initial mail UID is set to 1
+
+the sound file was obtained from http://www.freesound.org/people/Benboncan/sounds/91924/
---
+++ ching.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import imaplib
+import pygame
+import time
+import os.path
+
+imap_server   = "your_server"
+imap_port     = "993"
+imap_username = "username"
+imap_pw       = "pw"
+soundfile     = "sound.wav"
+
+def new_mail_generator(last_uid, host, port, login, password):
+    # connect
+    mail_server = imaplib.IMAP4_SSL(host, port)
+ 
+    # authenticate
+    try:
+        (retcode, capabilities) = mail_server.login(login, password)
+    except:
+        print sys.exc_info()[1]
+        sys.exit(1)
+    
+    #select the inbox
+    mail_server.select("INBOX")
+
+    #get all the email uids
+    result, data = mail_server.uid('search', None, "ALL")
+    
+    #write max uid to a file (persistent storage)
+    uid_file = open("email_uid", "w")
+    uid_file.write(max(data[0].split()))
+    uid_file.close()
+    
+    for message_uid in data[0].split():
+        if int(message_uid) > last_uid:
+             yield data[0][1]
+    
+def main():
+    
+    uid = int(1)
+
+    if os.path.isfile("email_uid"):
+        f = open("email_uid")
+        uid = int(f.readline())
+        f.close()
+    
+    for mail in new_mail_generator(uid,
+				host=imap_server,
+				port=imap_port,
+				login=imap_username,
+				password=imap_pw):
+        pygame.mixer.init()
+        pygame.mixer.music.load(soundfile)
+        pygame.mixer.music.play()
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
+
